70da9185e659cbe3bee8d37a4aca102e492a948c
jongho-chargev <jongho.chargev@gmail.com>
2024-11-15 13:02:41 +0900

    feat(ISSUE-7) 하하

--- dd.sh
+++ dd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-
+IS_USE_COMMENT=true
 # Notion API 토큰
 token="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"
 
@@ -8,7 +7,8 @@ token="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"
 database_id="13c5b5920009806b83b8e35d7d7eece5"
 
 # 현재 HEAD의 정보를 얻습니다
-commit_info=$(git show HEAD --pretty=format:"%d" --no-patch)
+commit_info=$(git show --pretty=format:"%d" --no-patch)
+author=$(git show HEAD --pretty=format:"%an" --no-patch)
 echo "commit_info: $commit_info\n"
 
 # 브랜치 이름 추출
@@ -20,6 +20,10 @@ echo "branches: $branches\n"
 # 필요한 브랜치 정보 추출
 develop_branch=$(echo "$branches" | grep -o 'develop' | head -n 1)
 feature_branch=$(echo "$branches" | grep -o 'ISSUE[^ ]*' | head -n 1)
+
+#현재 커밋 번호
+current_commit=$(git rev-parse HEAD)
+echo "current_commit $current_commit"
 
 # 결과 출력
 echo "Develop Branch: $develop_branch"
@@ -70,49 +74,78 @@ echo "response $response"
 # JSON 응답에서 Page ID 추출
 _page_id=$(echo "$response" | grep -o '"object":"page","id":"[^"]*' | sed 's/.*"id":"\([^"]*\)".*/\1/')
 _page_id=$(echo '{print "'$response'"}' | grep -o '"object":"page","id":"[^"]*' | sed 's/.*"id":"\([^"]*\)".*/\1/')
-
 page_id=$(echo '{print "'$_page_id'"}'| sed 's/.*"id":"\([^"]*\)".*/\1/' | sed 's/-//g')
 
 echo "page_id $page_id"
 
 requestWrite(){
-    #현재 커밋 번호
-    current_commit=$(git rev-parse HEAD)
-    echo "current_commit $current_commit"
+    url="https://api.notion.com/v1/blocks/$page_id/children"
+    echo "url $url"
 
-
-
-url="https://api.notion.com/v1/blocks/$page_id/children"
-echo "url $url"
-
-body='{
-    "children": [
-        {
-            "paragraph": {
-                "rich_text": [
-                    {
-                        "type": "text",
-                        "text": {
-                            "content": "소스가 반영되었습니다."
+    body='{
+        "children": [
+            {
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "소스가 반영되었습니다."
+                            }
                         }
-                    }
-                ]
+                    ]
+                }
+            },
+            {
+                "bookmark": {
+                    "url": "https://github.com/jongho-chargev/SampleFlutter/commit/'$current_commit'"
+                }
             }
-        },
-        {
-      "bookmark": {
-        "url": "https://github.com/jongho-chargev/SampleFlutter/commit/'$current_commit'"
-      }
-    }
-    ]
-}'
+        ]
+    }'
 
-res=$(curl -X PATCH "${header_args[@]}" -d "$body" "$url")
-echo $res
+    res=$(curl -X PATCH "${header_args[@]}" -d "$body" "$url")
+    echo $res
 }
 
+requestComment(){
+    url="https://api.notion.com/v1/comments"
+    echo "url $url"
 
-echo $(requestWrite)
+    body='{
+        "parent": {
+            "page_id": "'$page_id'"
+        },
+        "rich_text": [
+            {
+                "text": {
+                    "content": "[bot] 개발자 : '$author'\n"
+                }
+                
+            },
+            {
+                "text": {
+                    "content": "commit : '$current_commit'",
+                    "link" : {
+                        "url": "https://github.com/jongho-chargev/SampleFlutter/commit/'$current_commit'"
+                    }
+                }
+            }
+        ]
+    }'
+    echo "body $body\n"
+
+    res=$(curl -X POST "${header_args[@]}" -d "$body" "$url")
+    echo $res
+
+}
+# ...do something interesting...
+if [ "$IS_USE_COMMENT" = true ] ; then
+    echo $(requestComment)
+else
+    echo $(requestWrite)
+fi
+
 
 requestStatus(){
     url="https://api.notion.com/v1/pages/$page_id"
@@ -134,3 +167,5 @@ requestStatus(){
 }
 
 echo $(requestStatus)
+
+
