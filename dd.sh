97526fa58afc7371bb5a7efc53c96bc06cabbada
jongho-chargev <jongho.chargev@gmail.com>
2024-11-15 11:02:57 +0900

    feat(ISSUE-7) shell 완성

--- dd.sh
+++ dd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+# Notion API 토큰
+token="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"
+
+# 페이지 ID
+database_id="13c5b5920009806b83b8e35d7d7eece5"
+
 # 현재 HEAD의 정보를 얻습니다
 commit_info=$(git show HEAD --pretty=format:"%d" --no-patch)
 echo "commit_info: $commit_info\n"
@@ -17,3 +24,115 @@ feature_branch=$(echo "$branches" | grep -o 'ISSUE[^ ]*' | head -n 1)
 # 결과 출력
 echo "Develop Branch: $develop_branch"
 echo "Feature Branch: $feature_branch"
+
+
+
+
+# HTTP 요청 헤더
+headers=( 
+    "Authorization: Bearer $token" 
+    "Content-Type: application/json" 
+    "Notion-Version: 2022-06-28" 
+)
+header_args=() 
+for header in "${headers[@]}"; do 
+echo $header
+    header_args+=(-H "$header") 
+    echo "${header_args[@]}"
+done
+
+# HTTP 요청 URL
+
+requestQuery(){
+    issue_num=$(echo $feature_branch | grep -o '[0-9]\+')
+    echo "issue_num $issue_num"
+    
+
+    url_query="https://api.notion.com/v1/databases/$database_id/query"
+
+    # HTTP 요청 본문
+    data='{
+        "filter": {
+            "property": "ID",
+            "number": {
+                "equals": '$issue_num'
+            }
+        }
+    }'
+
+    # curl 명령 실행
+    echo "$headers" -d "$data" "$url_query"
+    res=$(curl -X POST "${header_args[@]}" -d "$data" "$url_query")
+    echo $res
+}
+
+response=$(requestQuery)
+echo "response $response"
+
+# JSON 응답에서 Page ID 추출
+_page_id=$(echo "$response" | grep -o '"object":"page","id":"[^"]*' | sed 's/.*"id":"\([^"]*\)".*/\1/')
+_page_id=$(echo '{print "'$response'"}' | grep -o '"object":"page","id":"[^"]*' | sed 's/.*"id":"\([^"]*\)".*/\1/')
+
+page_id=$(echo '{print "'$_page_id'"}'| sed 's/.*"id":"\([^"]*\)".*/\1/' | sed 's/-//g')
+
+echo "page_id $page_id"
+
+requestWrite(){
+    #현재 커밋 번호
+    current_commit=$(git rev-parse HEAD)
+    echo "current_commit $current_commit"
+
+
+
+url="https://api.notion.com/v1/blocks/$page_id/children"
+echo "url $url"
+
+body='{
+    "children": [
+        {
+            "paragraph": {
+                "rich_text": [
+                    {
+                        "type": "text",
+                        "text": {
+                            "content": "소스가 반영되었습니다."
+                        }
+                    }
+                ]
+            }
+        },
+        {
+      "bookmark": {
+        "url": "https://github.com/jongho-chargev/SampleFlutter/commit/'$current_commit'"
+      }
+    }
+    ]
+}'
+
+res=$(curl -X PATCH "${header_args[@]}" -d "$body" "$url")
+echo $res
+}
+
+
+echo $(requestWrite)
+
+requestStatus(){
+    url="https://api.notion.com/v1/pages/$page_id"
+
+    body='{
+        "properties": {
+            "상태" :{
+                "type": "status",
+                "status": {
+                    "name": "개발완료"
+                }
+            }
+        }
+    }'
+    
+    echo "url $url"
+    res=$(curl -X PATCH "${header_args[@]}" -d "$body" "$url")
+    echo $res
+}
+
+echo $(requestStatus)
